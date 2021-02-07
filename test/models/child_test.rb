c1768d977091f90916916a5a253398778b428399
Raymond <rli3@andrew.cmu.edu>
2021-02-07 11:56:56 -0500

    added tests

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -2,9 +2,47 @@ require 'test_helper'
 
 class ChildTest < ActiveSupport::TestCase
   # TODO: Relationship macros
-  
+  should have_many(:chores)
+  should have_many(:tasks).through(:chores)
   # TODO: Validation macros
-
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
   # TODO: Context testing
+  context "Creating a child context" do
+    setup do
+      create_children
+      create_tasks
+      create_chores
+    end
+ 
+    teardown do
+      # destroy_children
+      # destroy_tasks
+      destroy_chores
+      destroy_tasks
+      destroy_children
+    end
+ 
+    should "have name methods that list first_ and last_names combined" do
+      assert_equal "Alex Heimann", @alex.name
+      assert_equal "Mark Heimann", @mark.name
+      assert_equal "Rachel Heimann", @rachel.name
+      assert_not_equal "alex Heinmann", @alex.name
+    end
+ 
+    should "have a scope to alphabetize children" do
+      assert_equal ["Alex", "Mark", "Rachel"], Child.alphabetical.map{|c| c.first_name}
+    end
+ 
+    should "have a scope to select only active children" do
+      assert_equal ["Alex", "Mark"], Child.active.alphabetical.map{|c| c.first_name}
+    end
 
+    should "have points earned method in order to show number of points a child has" do
+      assert_equal 4, @alex.points_earned
+      assert_not_equal 1, @alex.points_earned
+      assert_equal 1, @mark.points_earned
+      assert_equal 0, @rachel.points_earned
+    end
+  end
 end
